71d0a9415c8d6185e72b100b5726fdc12074b509
Arnold deVos <adv+gh@backgroundsignal.com>
2024-09-17 00:05:26 +1000

    proposed compaction semi-group

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,16 @@ use std::hash::Hash;
 /// The message type of a `Syndicate` must provide a compaction key via this trait.
 pub trait Compactable
 where
+    Self: Sized,
     Self::Key: Eq + Hash,
 {
     type Key;
 
     /// The compaction key for this element.
     fn compaction_key(&self) -> Self::Key;
+
+    /// combine two elements
+    fn compact(self, _other: Self) -> Self {
+        self
+    }
 }
